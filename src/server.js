405ee6fc9c2088da1e3da24293b69a01950505a8
yohannesm <jonmersha@gmail.com>
2023-07-17 16:10:47 +0300

    home work

--- src/server.js
+++ src/server.js
@@ -16,6 +16,34 @@ app.use("/plan", planRoutes);
 const settingRoutes = require("./routes/system_setting");
 app.use("/setting", settingRoutes);
 
+//Audit Object
+const auditObject = require("./routes/audit_object");
+app.use("/audit", auditObject);
+
+//Audit Object
+const riskManagement = require("./routes/risk_management");
+app.use("/risk", riskManagement);
+
+//Audit Object
+const orgStructure = require("./routes/org_structure");
+app.use("/org", orgStructure);
+
+
+//User Managemnt
+const userManagement = require("./routes/user_routes");
+app.use("/user", userManagement);
+
+
+//Reporting
+const reporting = require("./routes/reporting");
+app.use("/report", reporting);
+
+//Audit Project
+const project = require("./routes/project");
+app.use("/project", project);
+
+
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
